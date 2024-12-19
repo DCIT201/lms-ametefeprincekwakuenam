f1342fe9111e1b6b93d703cfaeda9b78cbe995b9
ametefeprincekwakuenam <paksmith09@gmail.com>
2024-12-19 11:59:32 +0000

    Update App.java

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,14 +1,18 @@
-/*
- * This should be your main class where all your objects will be created
- */
-package org.example;
-
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
-
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        Library library = new Library();
+        
+        // Add books
+        library.addBook(new Book("1", "Java Basics", "Author A"));
+        library.addBook(new Book("2", "Advanced Java", "Author B"));
+
+        // Add patrons
+        Patron patron = new Patron("P1", "John Doe");
+        library.addPatron(patron);
+
+        // Borrow and return books
+        library.lendBook("1", "P1");
+        library.displayAvailableBooks();
+        library.returnBook("1", "P1");
     }
 }
